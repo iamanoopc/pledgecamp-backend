d7a4aafabdf028929ac00af185b734b934eb1da2
iamanoopc <mailto.anoopc@gmail.com>
2020-04-15 21:32:48 +0800

    heroku deploy setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const mockdata = require("./mockdata.js")
 
